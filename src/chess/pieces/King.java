549d0b157b0435edf96952eac526844d59495d9f
Bruno <bru-a@hotmail.com>
2023-09-01 16:30:52 -0300

    Possible moves of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -9,4 +9,10 @@ public class King extends ChessPiece{
 	public King(Board board, Color color) {
 		super(board, color);
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -9,4 +9,10 @@ public class Rook extends ChessPiece{
 	public Rook(Board board, Color color) {
 		super(board, color);
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
