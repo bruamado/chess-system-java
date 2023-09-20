c2e06250e32835b9ae2b244dc784b8104ed0f7ae
Bruno <bru-a@hotmail.com>
2023-09-20 10:21:39 -0300

    Bishop

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -8,6 +9,21 @@ public class Knight extends ChessPiece{
 
 	public Knight(Board board, Color color) {
 		super(board, color);
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position p = new Position(0, 0);
+		
+		//up-right
+		for (int i=0; i<3; i++) {
+			if (i==0) {
+				p.set
+			}
+		}
+		
+		return mat;
 	}
 	
 }
