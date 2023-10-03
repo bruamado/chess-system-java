348a135232721b130a10ede125262208a28bc0c1
Bruno <bru-a@hotmail.com>
2023-10-03 10:28:44 -0300

    fix printBoard with indexes colorized

--- src/application/UI.java
+++ src/application/UI.java
@@ -81,13 +81,13 @@ public class UI {
 	
 public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i=0; i<pieces.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print(ANSI_GREEN + (8 - i) + " " + ANSI_RESET);
 			for (int j=0; j<pieces.length; j++) {
 				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
+		System.out.println(ANSI_GREEN + "  a b c d e f g h" + ANSI_RESET);
 	}
 	
 	private static void printPiece(ChessPiece piece, boolean background) {
